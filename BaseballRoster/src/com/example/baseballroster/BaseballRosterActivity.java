b28467b70ab161b99150afdd71cbac8035143a9a
Joe Fearnley <joe.fearnley@gmail.com>
2013-09-17 12:43:12 -0400

    Initial commit of code for round 1 challenge - Create a baseball roster

---
+++ BaseballRoster/src/com/example/baseballroster/BaseballRosterActivity.java
@@ -0,0 +1,38 @@
+package com.example.baseballroster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class BaseballRosterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_baseball_roster);
+	
+		ListView listView = (ListView) findViewById(R.id.baseballRosterView);
+		List<String> players = new ArrayList<String>();
+		players.add("Orel Hershiser");
+		players.add("Kirk Gibson");
+		players.add("John Kruk");
+		players.add("Dwight Gooden");
+		players.add("Greg Maddux");
+		
+		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.baseball_roster_list_item, players);
+		listView.setAdapter(arrayAdapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.baseball_roster, menu);
+		return true;
+	}
+
+}
