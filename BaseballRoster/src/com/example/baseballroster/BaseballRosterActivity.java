4aa284db7e55860690d703ab060f028bff1d55ba
Joe Fearnley <joe.fearnley@gmail.com>
2013-09-17 19:32:57 -0400

    Adding action bar and new player screen

--- BaseballRoster/src/com/example/baseballroster/BaseballRosterActivity.java
+++ BaseballRoster/src/com/example/baseballroster/BaseballRosterActivity.java
@@ -5,16 +5,19 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class BaseballRosterActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_baseball_roster);
+		setContentView(R.layout.baseball_roster_activity);
 
 		ListView listView = (ListView) findViewById(R.id.baseballRosterView);
 		List<String> players = new ArrayList<String>();
@@ -23,15 +26,30 @@ public class BaseballRosterActivity extends Activity {
 		players.add("John Kruk");
 		players.add("Dwight Gooden");
 		players.add("Greg Maddux");
-		
+
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.baseball_roster_list_item, players);
 		listView.setAdapter(arrayAdapter);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.new_player:
+			Intent intent = new Intent(this, NewPlayerActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.action_settings:
+			Toast.makeText(this, "Hello! I'm Settings!", Toast.LENGTH_LONG).show();
+			break;
+		}
+		
+		return false;
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.baseball_roster, menu);
+		getMenuInflater().inflate(R.menu.baseball_roster_menu, menu);
 		return true;
 	}
 
---
+++ BaseballRoster/src/com/example/baseballroster/NewPlayerActivity.java
@@ -0,0 +1,40 @@
+package com.example.baseballroster;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NewPlayerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_player_activity);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.new_player:
+				Toast.makeText(this, "Hello! I'm Settings!", Toast.LENGTH_LONG).show();
+				break;
+			case R.id.action_settings:
+				Intent intent = new Intent(this, BaseballRosterActivity.class);
+				startActivity(intent);
+				return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.baseball_roster_menu, menu);
+		return true;
+	}
+
+}
