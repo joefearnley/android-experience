fe4dcb32e90bb329026117f30ca8330dcf86aa70
Joe Fearnley <joe.fearnley@gmail.com>
2013-09-23 18:48:13 -0400

    Initial commit of Config enum

---
+++ BaseballRoster/src/com/joefearnley/baseballroster/config/Config.java
@@ -0,0 +1,16 @@
+package com.joefearnley.baseballroster.config;
+
+public enum Config {
+	
+	STACKMOB_PUBLIC_KEY("");
+	
+    private String text;
+	
+	Config(String text) {
+		this.text = text;
+	}
+
+    public String toString() {
+    	return this.text;
+    }
+}
