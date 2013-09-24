0f545c808c1176c9b634e714b8cd9eb05e3368d7
Joe Fearnley <joe.fearnley@gmail.com>
2013-09-23 21:36:47 -0400

    Sure...go right ahead.

--- BaseballRoster/src/com/joefearnley/baseballroster/config/Config.java
+++ BaseballRoster/src/com/joefearnley/baseballroster/config/Config.java
@@ -2,7 +2,7 @@ package com.joefearnley.baseballroster.config;
 
 public enum Config {
 
-	STACKMOB_PUBLIC_KEY("");
+	STACKMOB_PUBLIC_KEY("5ec4cc67-3631-4f1c-97f4-a88955b02cef");
 	
 	private String text;
 	
