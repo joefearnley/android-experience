ee1d47a1265df101bec84f3a4db70dac21fe26a9
Joe Fearnley <joe.fearnley@gmail.com>
2013-09-25 21:28:50 -0400

    Updating config

--- BaseballRoster/src/com/joefearnley/baseballroster/config/Config.java
+++ BaseballRoster/src/com/joefearnley/baseballroster/config/Config.java
@@ -2,7 +2,7 @@ package com.joefearnley.baseballroster.config;
 
 public enum Config {
 
-	STACKMOB_PUBLIC_KEY("5ec4cc67-3631-4f1c-97f4-a88955b02cef");
+	STACKMOB_PUBLIC_KEY("");
 	
 	private String text;
 	
